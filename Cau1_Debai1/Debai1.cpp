b32cfc3f069f553206a700ba8486f56714513026
dongpfiev2014 <dongpfiev2014@gmail.com>
2024-01-22 18:59:43 +0700

    first time

---
+++ Cau1_Debai1/Debai1.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 1;
+    cout << "Hello World!! " << a << endl;
+    return 0;
+}
---
+++ Cau2_Debai2/Debai2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 3;
+    cout << "Hello World!! " << a << endl;
+    return 0;
+}
